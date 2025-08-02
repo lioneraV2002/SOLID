f77d9f1cad8ea0dff84e464d34b65cb75ad0ccfe
SivashMomeni <momeni.sm79@gmail.com>
2025-08-02 15:39:00 +0330

    Add empty sendTelegramMessage to SmsMessageService and EmailMessageService

--- step2-with-solid/src/main/java/org/example/services/EmailMessageService.java
+++ step2-with-solid/src/main/java/org/example/services/EmailMessageService.java
@@ -1,4 +1,9 @@
 package org.example.services;
+import org.example.models.TelegramMessage;
 
 public class EmailMessageService implements EmailService {
+    @Override
+    public void send(TelegramMessage telegramMessage) {
+        // Empty implementation since SMS service doesn't handle Telegram messages
+    }
 }
--- step2-with-solid/src/main/java/org/example/services/SmsMessageService.java
+++ step2-with-solid/src/main/java/org/example/services/SmsMessageService.java
@@ -1,4 +1,10 @@
 package org.example.services;
+import org.example.models.TelegramMessage;
 
 public class SmsMessageService implements SmsService{
+    @Override
+    public void send(TelegramMessage telegramMessage) {
+        // Empty implementation since SMS service doesn't handle Telegram messages
+    }
+
 }
