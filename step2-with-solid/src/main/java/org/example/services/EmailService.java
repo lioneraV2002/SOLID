208d81a0a61129a7debc4a7bd87168da76bbff07
bayat <mohammad-bayat-1381@outlook.com>
2025-08-02 14:03:36 +0330

    Update Service Interfaces issue 44

--- step2-with-solid/src/main/java/org/example/services/EmailService.java
+++ step2-with-solid/src/main/java/org/example/services/EmailService.java
@@ -3,4 +3,5 @@ package org.example.services;
 import org.example.models.EmailMessage;
 
 public interface EmailService extends MessageSender<EmailMessage> {
+    void send(EmailMessage emailMessage);
 }
--- step2-with-solid/src/main/java/org/example/services/TelegramService.java
+++ step2-with-solid/src/main/java/org/example/services/TelegramService.java
@@ -3,4 +3,5 @@ package org.example.services;
 import org.example.models.TelegramMessage;
 
 public interface TelegramService extends MessageSender<TelegramMessage> {
+    void send(TelegramMessage telegramMessage);
 }
